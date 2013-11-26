2637144d2bc9f7a0871a1857c6147d1d35659b55
mariolpantunes <mariolpantunes@gmail.com>
2013-11-26 17:17:22 +0000

    Fix JabRef package.

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # sleep 3
 setxkbmap -layout pt
-xbacklight -set 25
-host gaia.av.it.pt
+#xbacklight -set 25
+echo 5 | sudo tee /sys/class/backlight/asus_laptop/brightness
+
+host "gaia.av.it.pt"
 INSIDE_IT="$?"
 if [[ $INSIDE_IT -eq 0 ]]; then
     sudo /sbin/route add -net 10.115.0.0 netmask 255.255.0.0 gw gaia.av.it.pt
